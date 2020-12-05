3a5b55fe16ee4dad83a13b06ca7fb3ca55da9066
Yalcin Erdemir <yedort@hotmail.com>
2020-12-05 17:04:59 +0000

    Create mousekeys.sh

---
+++ desktop-commands/mousekeys.sh
@@ -0,0 +1,5 @@
+# Ubuntu 16.04 or below
+gsettings set org.gnome.desktop.a11y.keyboard mousekeys-enable true
+gsettings set org.gnome.settings-daemon.plugins.cursor active false
+gsettings set org.gnome.desktop.a11y.keyboard mousekeys-max-speed 4000
+gsettings set org.gnome.desktop.a11y.keyboard mousekeys-init-delay 120
