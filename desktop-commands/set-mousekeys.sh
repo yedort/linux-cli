b32c970d6fe4d8f5d65dd07e81e2172c0e50415b
Yalcin Erdemir <akasoft.aka@gmail.com>
2021-01-12 18:48:52 +0400

    Update set-mousekeys.sh

--- desktop-commands/set-mousekeys.sh
+++ desktop-commands/set-mousekeys.sh
@@ -1,5 +1,5 @@
 # Ubuntu 16.04 or below
 gsettings set org.gnome.desktop.a11y.keyboard mousekeys-enable true
 gsettings set org.gnome.settings-daemon.plugins.cursor active false
-gsettings set org.gnome.desktop.a11y.keyboard mousekeys-max-speed 1000
+gsettings set org.gnome.desktop.a11y.keyboard mousekeys-max-speed 500
 gsettings set org.gnome.desktop.a11y.keyboard mousekeys-init-delay 0
